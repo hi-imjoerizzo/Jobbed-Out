e233b597d1dbf8e3a346f5cab54aad08c9c7eeff
David Keathley <keathleydavidj@gmail.com>
2016-02-19 16:24:16 -0600

    4th user story knocked out

--- public/javascripts/jobsController.js
+++ public/javascripts/jobsController.js
@@ -44,6 +44,52 @@
       });
     }
 
+    this.searchJobs = function(event) {
+      event.preventDefault();
+
+      var self = this;
+
+      var title = $scope.searchTitle;
+      var company = $scope.searchCompany;
+
+      $http({
+        method: 'GET',
+        url: api,
+        params: {
+          title: title,
+          company: company
+        }
+      }).then(function successCallback(response) {
+        console.log("success", response.data);
+
+        self.data = response.data;
+        currentPage = response.data.page;
+      }, function errorCallback(response) {
+        console.log("hugeFuckingFailure: ", response.data);
+
+      });
+    }
+
+    this.sortJobs = function(sort) {
+      var self = this;
+
+      $http({
+        method: 'GET',
+        url: api,
+        params: {
+          sort: sort
+        }
+      }).then(function successCallback(response) {
+        console.log("success", response.data);
+
+        self.data = response.data;
+        currentPage = response.data.page;
+      }, function errorCallback(response) {
+        console.log("hugeFuckingFailure: ", response.data);
+
+      });
+    }
+
     return this;
   });
 })();
