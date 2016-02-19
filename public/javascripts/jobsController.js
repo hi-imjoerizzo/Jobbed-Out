4a9f7f5641bfd520b3f501b952e3f80ca8d2f071
David Keathley <keathleydavidj@gmail.com>
2016-02-19 14:59:15 -0600

    click button AJAX

--- public/javascripts/jobsController.js
+++ public/javascripts/jobsController.js
@@ -2,21 +2,45 @@
   'use strict';
 
   var app = angular.module('JobbedOut');
+  var api = '/api/v1/jobs/';
 
   app.controller("JobsController", function($scope, $http) {
     var self = this;
+    var currentPage;
 
     $http({
       method: "GET",
-      url: "/api/v1/jobs"
+      url: api
     }).then(function successCallback(response) {
-      console.log("success", response.data.docs);
+      console.log("success", response.data);
 
-      self.jobs = response.data.docs;
+      self.data = response.data;
+      currentPage = response.data.page;
     }, function errorCallback(response) {
       console.log("hugeFuckingFailure: ", response.data);
 
     });
+
+    this.loadMore = function() {
+      var self = this;
+
+      $http({
+        method: 'GET',
+        url: api,
+        data: {
+          page: currentPage + 1
+        }
+      }).then(function successCallback(response) {
+        console.log("success", response.data);
+
+        self.data = response.data;
+        currentPage = response.data.page;
+      }, function errorCallback(response) {
+        console.log("hugeFuckingFailure: ", response.data);
+
+      });
+    }
+
     return this;
   });
 })();
